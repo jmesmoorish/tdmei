05c701b8188c6443569f1fa29ec5b9461222c406
tamoura <1160477@isep.ipp.pt>
2020-06-09 00:07:41 +0100

    pequena limpeza.

--- r-code/dupe-files.R
+++ r-code/dupe-files.R
@@ -5,13 +5,14 @@ library(digest)
 library(stringr)
 library(filesstrings)
 
-test_dir= "D:/Dropbox/0_ISEP/TDMEI/collectSmartContractsFromEtherscan/collectedContractsOriginal"
-filelist <- dir(test_dir, pattern = ".sol", recursive=TRUE, all.files =TRUE, full.names=TRUE)
+folder1 <- "D:/Dropbox/0_ISEP/TDMEI/collectSmartContractsFromEtherscan/collectedContractsOriginal"
+folder2 <- "D:/Dropbox/0_ISEP/TDMEI/collectSmartContractsFromEtherscan/duplicated_contracts"
+filelist <- dir(folder, pattern = ".sol", recursive=TRUE, all.files =TRUE, full.names=TRUE)
 
 md5s <- sapply(filelist,digest,file=TRUE,algo="md5", length = 5000)
 duplicate_files <- split(filelist,md5s)
 
-# now let's divide the list into duplicates ( length > 1) and uniques ( length - 1)
+#now divide the list into duplicates ( length > 1) and uniques ( length - 1)
 z <- duplicate_files
 z2 <- sapply(z,function (x){length(x)>1})
 z3 <- split(z,z2)
@@ -21,7 +22,7 @@ dupes <- z3$"TRUE"
 for (i in 1:length(dupes)){ 
   for (j in 1:length(dupes[[i]][])){
     if (j > 1){
-      file.move(dupes[[i]][j], "D:/Dropbox/0_ISEP/TDMEI/collectSmartContractsFromEtherscan/duplicated_contracts")
+      file.move(dupes[[i]][j], folder2)
     }
   }
 }
